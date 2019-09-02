a4233ea6f3fa6deacbad5869ec28a4e4018e60d1
saifowoo <saif.eldeen.moustafaa@gmail.com>
2019-09-02 15:58:26 +0200

    Project Old Version

---
+++ MISRA Challenge/Debug/L3G4200D_Lcfg.d
@@ -0,0 +1,27 @@
+# FIXED
+
+L3G4200D_Lcfg.obj: ../L3G4200D_Lcfg.c
+L3G4200D_Lcfg.obj: ../L3G4200D_Lcfg.h
+L3G4200D_Lcfg.obj: ../Basic_Types.h
+L3G4200D_Lcfg.obj: ../L3G4200D_Cfg.h
+L3G4200D_Lcfg.obj: ../Basic_Types.h
+L3G4200D_Lcfg.obj: ../DIO.h
+L3G4200D_Lcfg.obj: ../Basic_Types.h
+L3G4200D_Lcfg.obj: ../Basic_Types.h
+L3G4200D_Lcfg.obj: ../SPI.h
+L3G4200D_Lcfg.obj: ../Basic_Types.h
+L3G4200D_Lcfg.obj: ../Basic_Types.h
+L3G4200D_Lcfg.obj: ../L3G4200D.h
+
+../L3G4200D_Lcfg.c: 
+../L3G4200D_Lcfg.h: 
+../Basic_Types.h: 
+../L3G4200D_Cfg.h: 
+../Basic_Types.h: 
+../DIO.h: 
+../Basic_Types.h: 
+../Basic_Types.h: 
+../SPI.h: 
+../Basic_Types.h: 
+../Basic_Types.h: 
+../L3G4200D.h: 
---
+++ MISRA Challenge/Debug/uart_test.d
@@ -0,0 +1,15 @@
+# FIXED
+
+uart_test.obj: ../uart_test.c
+uart_test.obj: ../Basic_Types.h
+uart_test.obj: ../BLMGR.h
+uart_test.obj: ../PWM.h
+uart_test.obj: ../BasicTypes.h
+uart_test.obj: ../GPT.h
+
+../uart_test.c: 
+../Basic_Types.h: 
+../BLMGR.h: 
+../PWM.h: 
+../BasicTypes.h: 
+../GPT.h: 
