371f11aa37e69c3ce3ec4db875a5a2a79058dab5
Saif El-Deen <saif.eldeen.moustafaa@gmail.com>
2019-09-10 19:54:16 +0200

    Update L3G4200D_Cfg.h

--- MISRA Challenge/L3G4200D_Cfg.h
+++ MISRA Challenge/L3G4200D_Cfg.h
@@ -13,28 +13,36 @@
 #include "SPI.h"
 
 /*SPI Communication Configuration*/
-#define GYHD_INIT_SLAVE_SELECT() DIO_InitPortDirection(PB,1<<4,1<<4);\
-                                DIO_WritePort(PB,1<<4,1<<4) 							
-#define GYHD_ACTIVATE_SLAVE_SELECT() DIO_WritePort(PB,~(1<<4),1<<4)
-#define GYHD_DEACTIVATE_SLAVE_SELECT() DIO_WritePort(PB,(1<<4),(1<<4))
-
+void GYHD_INIT_SLAVE_SELECT(void);
+void GYHD_ACTIVATE_SLAVE_SELECT(void);
+void GYHD_DEACTIVATE_SLAVE_SELECT(void);
+void _delay_ms(u32 delay);
+/*
+#define GYHD_INIT_SLAVE_SELECT()        DIO_InitPortDirection(PB,1<<4,1<<4);\
+        DIO_WritePort(PB,1<<4,1<<4)
+#define GYHD_ACTIVATE_SLAVE_SELECT()    DIO_WritePort(PB,~(1<<4),1<<4)
+#define GYHD_DEACTIVATE_SLAVE_SELECT()  DIO_WritePort(PB,(1<<4),(1<<4))
+*/
 /*Timeout Management Configuration*/
-#define u8USE_DELAY 0x00
-#define u8USE_TIMER 0x01
+#define u8USE_DELAY (0x00u)
+#define u8USE_TIMER (0x01u)
 
-#define u8TIMEOUT_FUNCTION u8USE_DELAY
+#define u8TIMEOUT_FUNCTION (u8USE_DELAY)
 
 #if(u8TIMEOUT_FUNCTION == u8USE_DELAY)
 
-
+void u8START_TIME_OUT_MS(u32 DELAY_MS,u8 * FLAG_PTR);
+/*
 #define u8START_TIME_OUT_MS(DELAY_MS,FLAG_PTR) _delay_ms(DELAY_MS);\
-                                              *(FLAG_PTR) = 0x01
+        *(FLAG_PTR) = 0x01u
+*/
 #else
 #define u8START_TIME_OUT_MS(DELAY_MS,FLAG_PTR)
 #endif
 
 /*Self Axis Movement Detection Config*/
-#define u8SELF_AXIS_MOV  ON
+#define ON 1u
+#define u8SELF_AXIS_MOV  (ON)
 
 
 #endif /* L3G4200D_CFG_H_ */
